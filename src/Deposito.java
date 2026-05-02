11ad929bac4a79c2f329196b2ff195ea18a8d28d
felipe <felipazo337@gmail.com>
2026-05-02 18:54:13 -0400

    docs: agregar Javadoc a Catalogo y Deposito

--- src/Deposito.java
+++ src/Deposito.java
@@ -9,12 +9,25 @@ import java.util.ArrayList;
 public class Deposito<T> {
     private ArrayList<T> almacen;
 
+    /**
+     * Constructor que inicializa el depósito vacío.
+     */
     public Deposito(){
         this.almacen = new ArrayList<T>();
     }
+
+    /**
+     * Agrega un elemento al deposito.
+     * @param item el elemento a agregar
+     */
     public void add(T item){
         this.almacen.add(item);
     }
+
+    /**
+     * Retorna el primer elemento del deposito
+     * @return el primer elemento, o null si el deposito está vacío.
+     */
     public T get(){
         if(almacen.isEmpty()) return null;
         return almacen.remove(0);

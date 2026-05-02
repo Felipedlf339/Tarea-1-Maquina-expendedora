7663ebeec05910c9967ad6c2503a4484f519fd60
felipe <felipazo337@gmail.com>
2026-05-01 23:28:56 -0400

    fix: corrección de sintaxis en Catalogo

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public enum Catalogo {
-    COCACOLA(1, 100);
-    SPRITE(2, 1000);
-    FANTA(3, 1000);
-    SNICKERS(4, 800);
+    COCACOLA(1, 100),
+    SPRITE(2, 1000),
+    FANTA(3, 1000),
+    SNICKERS(4, 800),
     SUPER8(5, 500);
 
     private final int id;

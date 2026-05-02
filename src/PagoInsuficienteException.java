5ac0c62b000880a129fad1aecdd70dc7f4556d04
felipe <felipazo337@gmail.com>
2026-05-02 18:54:46 -0400

    docs: agregar Javadoc a excepciones personalizadas

--- src/PagoInsuficienteException.java
+++ src/PagoInsuficienteException.java
@@ -1,6 +1,15 @@
 package org.example;
 
+/**
+ * Excepcion lanzada en caso de el valor de la moneda sea
+ * menor al precio del producto solicitado
+ */
+
 public class PagoInsuficienteException extends Exception{
+    /**
+     * Constructor con el mensaje descriptivo del error
+     * @param mensaje descripcion del motivo de la excepcion.
+     */
     public PagoInsuficienteException(String mensaje){
         super(mensaje);
     }

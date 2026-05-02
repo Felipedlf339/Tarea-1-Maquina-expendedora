dec9cda6f3536f6feddd636ab4f88676bc04d16d
javvivie <jaaravena2025@udec.cl>
2026-05-02 18:25:09 -0400

    feat: actualización de la clase Comprador + documentación javadoc de Comprador

--- src/src/org/example/Comprador.java
+++ src/src/org/example/Comprador.java
@@ -1,32 +1,41 @@
 package org.example;
 
+/**
+ * Clase que actúa como un comprador.
+ * Compra, recibe el vuelto y consume el producto.
+ */
 public class Comprador {
-
+    /**
+     * Sabor del producto consumido.
+     */
     private String sabor;
+
+    /**
+     * Cantidad total de vuelto recibido.
+     */
     private int totalVuelto;
 
-    public Comprador(Moneda moneda, int numProducto, Expendedor expendedor)
+    /**
+     * Constructor del Comprador que realiza la compra del producto.
+     * @param moneda Moneda con la que se hace el pago.
+     * @param cual Que producto del catálogo se quiere comprar.
+     * @param expendedor Referencia al expendedor donde se realiza la compra.
+     * @throws PagoIncorrectoException Si la moneda es null.
+     * @throws NoHayProductoException  Si no se encuentra dispoible el producto.
+     * @throws PagoInsuficienteException Si el valor de la moneda no es suficiente para completar el pago.
+     */
+    public Comprador(Moneda moneda, Catalogo cual, Expendedor expendedor)
             throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
 
         this.sabor = null;
         this.totalVuelto = 0;
 
-        Catalogo cual = null;
-        Catalogo[] productos = Catalogo.values();
-        for (int i = 0; i < productos.length; i++) {
-            if (productos[i].getId() == numProducto) {
-                cual = productos[i];
-                break;
-            }
-        }
-
-        if (cual == null) {
-            throw new NoHayProductoException("No existe producto que tenga el ID: " + numProducto);
-        }
-
+        // Intenta comprar el producto del expendedor.
         Producto producto = expendedor.comprarProducto(moneda, cual);
+        // Se consume el producto y se guarda su sabor en una cadena de texto.
         this.sabor = producto.consumir();
 
+        // Saca las monedas del vuelto una a una y las almacena hasta no quedar ninguna.
         Moneda vuelto = expendedor.getVuelto();
         while (vuelto != null) {
             this.totalVuelto += vuelto.getValor();
@@ -34,10 +43,18 @@ public class Comprador {
         }
     }
 
+    /**
+     * Para obtener el nombre del producto consumido.
+     * @return retorna el sabor (nombre) del producto consumido.
+     */
     public String getSabor() {
         return sabor;
     }
 
+    /**
+     * Para obtener el total del vuelto recibido
+     * @return el total del vuelto en un entero.
+     */
     public int getTotalVuelto() {
         return totalVuelto;
     }

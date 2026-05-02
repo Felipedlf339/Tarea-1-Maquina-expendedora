6d87fa01de1e1f01f37b8125f330d0c4823134c4
javvivie <jaaravena2025@udec.cl>
2026-05-02 11:53:20 -0400

    feat: implementación de la clase Comprador

---
+++ src/src/org/example/Comprador.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Comprador {
+    private String sabor;
+    private int totalVuelto;
+
+    public Comprador (Moneda moneda, Catalogo cual, Expendedor expendedor)
+        throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException
+    {
+
+        this.sabor = null;
+        this.totalVuelto = 0;
+
+        Producto producto expendedor.comprarProducto(moneda, cual);
+
+        this.sabor = producto.consumir();
+
+        Moneda vuelto = expendedor.getVuelto();
+        while (vuelto != null) {
+            this.totalVuelto += vuelto.getValor();
+            vuelto = expendedor.getVuelto();
+
+        }
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public int getTotalVuelto() {
+        return totalVuelto;
+    }
+
+
+
+}

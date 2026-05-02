0ad0dbcc7272e548df84c553c84d320da5e57224
javvivie <jaaravena2025@udec.cl>
2026-05-02 13:52:28 -0400

    docs: se añadió documentación Javadoc a las clases Super8 y Snickers.

--- src/src/org/example/Snickers.java
+++ src/src/org/example/Snickers.java
@@ -1,10 +1,23 @@
 package org.example;
 
+/**
+ * Clase que representa un Snicker.
+ * Hereda de Dulce e implenta consumir.
+ */
 public class Snickers extends Dulce {
+
+    /**
+     * Constructor que inicializa un Snickers con su número de serie.
+     * @param serie número de serie del Snickers.
+     */
     public Snickers(int serie) {
-        super(serie);
+        super(serie); // Llama al constructor de la clase madre Dulce.
     }
 
+    /**
+     * Implementación de consumir un Snickers.
+     * @return una cadena de texto que indica el producto consumido.
+     */
     @Override
     public String consumir() {
         return "snickers";

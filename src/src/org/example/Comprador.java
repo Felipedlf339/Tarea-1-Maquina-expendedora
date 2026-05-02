71beac2f2acfb43e491de198cc83b2510f953b1e
javvivie <jaaravena2025@udec.cl>
2026-05-02 14:23:42 -0400

    feat: actualización de la clase Comprador

--- src/src/org/example/Comprador.java
+++ src/src/org/example/Comprador.java
@@ -5,11 +5,24 @@ public class Comprador {
     private String sabor;
     private int totalVuelto;
 
-    public Comprador(Moneda moneda, Catalogo cual, Expendedor expendedor)
+    public Comprador(Moneda moneda, int numProducto, Expendedor expendedor)
             throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
 
         this.sabor = null;
         this.totalVuelto = 0;
+
+        Catalogo cual = null;
+        Catalogo[] productos = Catalogo.values();
+        for (int i = 0; i < productos.length; i++) {
+            if (productos[i].getId() == numProducto) {
+                cual = productos[i];
+                break;
+            }
+        }
+
+        if (cual == null) {
+            throw new NoHayProductoException("No existe producto que tenga el ID: " + numProducto);
+        }
 
         Producto producto = expendedor.comprarProducto(moneda, cual);
         this.sabor = producto.consumir();
